ceadab0211f8339dc61e05c7e92f9ccef23a6852
incheckdev-glitch <incheckdev@gmail.com>
2026-07-28 14:07:07 +0300

    Load creative checklist creation styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import { AuthGate } from './components/AuthGate'
 import './styles.css'
 import './video-flow.css'
+import './creative-flow.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
