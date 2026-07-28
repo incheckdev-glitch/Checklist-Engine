453a6c41029a26089cea6014fb94b8696c2b0111
incheckdev-glitch <incheckdev@gmail.com>
2026-07-28 13:39:24 +0300

    Load video flow checklist builder styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { AuthGate } from './components/AuthGate'
 import './styles.css'
+import './video-flow.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
